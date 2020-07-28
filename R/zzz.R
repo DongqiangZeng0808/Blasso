8357720c5070ea48014982327f3e6348aeaa8b32
DongqiangZeng0808 <dongqiangzeng0808@gmail.com>
2020-07-28 12:33:46 +0800

    update function

--- R/zzz.R
+++ R/zzz.R
@@ -6,12 +6,12 @@
 .onAttach <- function(libname, pkgname) {
   pkgVersion <- packageDescription(pkgname, fields="Version")
   msg <- paste0(pkgname, " v", pkgVersion, "  ",
-                "For help: https://github.com/DongqiangZeng0808/", "Blasso", "\n\n")
+                "For help: https://github.com/DongqiangZeng0808/issues", "Blasso", "\n\n")
 
   citation <- paste0(" If you use ", pkgname, " in published research, please cite:\n",
                      "==========================================================================\n",
                      " Macrophage correlates with immunophenotype and predicts anti-PD-L1\n",
-                     " response of urothelial cancer. Theranostics 2020; 10(15):7002-7014.",
+                     " response of urothelial cancer. Theranostics 2020; 10(15):7002-7014.\n",
                      "==========================================================================")
 
   packageStartupMessage(paste0(msg, citation))
