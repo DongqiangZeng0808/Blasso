4522baf70713486d5a18ccc75062737f6220914a
DongqiangZeng0808 <dongqiangzeng0808@gmail.com>
2021-06-18 17:39:41 +0800

    update FUNCTIONS

--- R/zzz.R
+++ R/zzz.R
@@ -5,11 +5,13 @@
 ##' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname) {
   pkgVersion <- packageDescription(pkgname, fields="Version")
-  msg <- paste0(pkgname, " v", pkgVersion, "  ",
+  msg <- paste0("==========================================================================\n",
+                pkgname, " v", pkgVersion, "  ",
                 "For help: https://github.com/DongqiangZeng0808/", "Blasso", "\n\n")
 
-  citation <- paste0(" If you use ", pkgname, " in published research, please cite:\n",
-                     "==========================================================================\n",
+  citation <- paste0(
+                     " If you use ", pkgname, " in published research, please cite:\n",
+
                      " Macrophage correlates with immunophenotype and predicts anti-PD-L1\n",
                      " response of urothelial cancer. Theranostics 2020; 10(15):7002-7014.\n",
                      "==========================================================================")
